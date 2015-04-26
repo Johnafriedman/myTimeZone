f731ad703b41e972ead41f41092fe173af42e3cd
Johnafriedman <john@innovationsforlearning.org>
2015-04-26 15:19:30 -0500

    adding geolocation and SunCalc

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,33 +1,48 @@
 
 var myTimeZone = {
-    // Application Constructor
+
+    position: {},
+
+
     initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+        _.bindAll(this);
         myTimeZone.render();
+        myTimeZone.getLocation();
     },
 
     render: function(){
         var paper = Raphael(10, 50, 320, 200);
         var c = paper.circle(50, 50, 40);
+    },
+
+    getLocation: function(){
+        navigator.geolocation.getCurrentPosition(this.onGPSSuccess, this.onGPSError);
+    },
+
+    onGPSSuccess: function(position){
+        this.position = position;
+        alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+        var result = SunCalc.getTimes(new Date(),position.coords.latitude, position.coords.longitude);
+        console.log(result);
+    },
+
+    onGPSError: function(error){
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
     }
 
 };
 
-myTimeZone.initialize();
+document.addEventListener('deviceready', myTimeZone.initialize(), false);
 
+// determine latitude
 
 // Draw circle
 
