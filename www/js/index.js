86e662890baa5efff9ee2c3ff4002eb025d61e95
Johnafriedman <john@innovationsforlearning.org>
2015-05-02 12:41:56 -0500

    getting closer on myTime calculation

--- www/js/index.js
+++ www/js/index.js
@@ -1,54 +1,54 @@
 
 var myTimeZone = {
 
-    position: {},
-    paper: null,
+  position: {},
+  times: {},
+  offset: 0,
+  paper: null,
 
-    initialize: function() {
-        _.bindAll(this);
-        myTimeZone.render();
-        myTimeZone.getLocation();
-    },
+  initialize: function() {
+    _.bindAll(this);
+    this.paper = Raphael(10, 50, 1024, 768);
+    myTimeZone.getLocation();
+  },
 
-    render: function(){
-        this.paper = Raphael(10, 50, 1024, 768);
-        var c = this.paper.circle(50, 50, 40);
-        var path = this.paper.text(10, 10, "hello");;
-        this.paper.path(path);
+  render: function(){
+    this.paper.clear();
+    var c = this.paper.circle(50, 50, 40);
+    var s="";
+    _.forEach(this.times, function(d,k){
+        s=s+k+":"+d+"\n";
 
-    },
+    });
+    this.paper.text(200, 100, s);
 
-    getLocation: function(){
-        navigator.geolocation.getCurrentPosition(this.onGPSSuccess, this.onGPSError);
-    },
+  },
 
-    onGPSSuccess: function(position){
-        this.position = position;
-        // alert('Latitude: '          + position.coords.latitude          + '\n' +
-        //       'Longitude: '         + position.coords.longitude         + '\n' +
-        //       'Altitude: '          + position.coords.altitude          + '\n' +
-        //       'Accuracy: '          + position.coords.accuracy          + '\n' +
-        //       'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-        //       'Heading: '           + position.coords.heading           + '\n' +
-        //       'Speed: '             + position.coords.speed             + '\n' +
-        //       'Timestamp: '         + position.timestamp                + '\n');
-        var result = SunCalc.getTimes(new Date(),position.coords.latitude, position.coords.longitude);
-        var s="";
-        _.forEach(result, function(d,k){
-            s=s+k+":"+d+"\n";
+  getLocation: function(){
+    this.paper.text(200, 100, "identifying location");
+    navigator.geolocation.getCurrentPosition(this.onGPSSuccess, this.onGPSError);
+  },
 
-        });
-        var path = this.paper.text(200, 100, s);
-    },
+  onGPSSuccess: function(position){
+    this.position = position;
+    this.times = SunCalc.getTimes(new Date(),position.coords.latitude, position.coords.longitude);
+    var t = new Date().getTime() + (new Date().getTimezoneOffset()*60*1000); // UTC time
+    this.offset = position.coords.longitude*(24*60*60*1000/360); // mt = lat* ms per degree
+    this.times.myTime = new Date(t+this.offset);
 
-    onGPSError: function(error){
-        alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
-    }
+    this.render();
+  },
+
+  onGPSError: function(error){
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+  }
 
 };
 
 document.addEventListener('deviceready', myTimeZone.initialize(), false);
+
+mt = (360/24)*mlat
 
 // determine latitude
 
