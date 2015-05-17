b8f4e01d95da0bdc532e5d06dd25f2ea51d99dd7
Johnafriedman <john@innovationsforlearning.org>
2015-05-17 15:03:38 -0500

    refactoring calcCircle

--- www/js/circle.js
+++ www/js/circle.js
@@ -1,39 +1,116 @@
 
-CalcCircle(pt1, pt2, pt3)
+ 
+
+function circleFrom3Points (pt1,pt2,pt3)
 {
-  var yDelta_a= pt2.y - pt1.y;
-  var xDelta_a= pt2.x - pt1.x;
-  var yDelta_b= pt3.y - pt2.y;
-  var xDelta_b= pt3.x - pt2.x;
-  
-  if (Math.abs(xDelta_a) <= 0.000000001 && Math.abs(yDelta_b) <= 0.000000001){
-    console.log("Calc cirlce \n");
-    this.m_Center.m_x= 0.5*(pt2.x() + pt3.x());
-    this.m_Center.m_y= 0.5*(pt1.y() + pt2.y());
-    this.m_Center.m_z= pt1.z();
-    this.m_dRadius= length(&m_Center,pt1);   // calc. radius
-   console.log(" Center: %f %f %f\n", m_Center.x(), m_Center.y(), m_Center.z());
-   console.log(" radius: %f %f %f\n", length(&m_Center,pt1), length(&m_Center,pt2),length(&m_Center,pt3));
+  var center = {x:-1, y:-1};
+  var radius = -1;
 
-    return this.m_dRadius;
+    radius=-1;   // error checking 
+    
+    if (!isPerpendicular(pt1, pt2, pt3) )       calcCircle(pt1, pt2, pt3);  
+    else if (!isPerpendicular(pt1, pt3, pt2) )    calcCircle(pt1, pt3, pt2);  
+    else if (!isPerpendicular(pt2, pt1, pt3) )    calcCircle(pt2, pt1, pt3);  
+    else if (!isPerpendicular(pt2, pt3, pt1) )    calcCircle(pt2, pt3, pt1);  
+    else if (!isPerpendicular(pt3, pt2, pt1) )    calcCircle(pt3, pt2, pt1);  
+    else if (!isPerpendicular(pt3, pt1, pt2) )    calcCircle(pt3, pt1, pt2);  
+    else { 
+      console.log("\nThe three pts are perpendicular to axis\n");
+  //    pt1.console.log();     pt2.console.log();     pt3.console.log();
+      radius=-1;
+      return ;
+    }
+
+    return {center: center, radius: radius}
+
+
+  function isPerpendicular(pt1,pt2,pt3)
+  // Check the given point are perpendicular to x or y axis 
+  {
+    var yDelta_a= pt2.y - pt1.y;
+    var xDelta_a= pt2.x - pt1.x;
+    var yDelta_b= pt3.y - pt2.y;
+    var xDelta_b= pt3.x - pt2.x;
+    
+
+  //  console.log(" yDelta_a: %f xDelta_a: %f \n",yDelta_a,xDelta_a);
+  //  console.log(" yDelta_b: %f xDelta_b: %f \n",yDelta_b,xDelta_b);
+
+    // checking whether the line of the two pts are vertical
+    if (Math.abs(xDelta_a) <= 0.000000001 && Math.abs(yDelta_b) <= 0.000000001){
+      console.log("The points are pependicular and parallel to x-y axis\n");
+      return false;
+    }
+
+    if (Math.abs(yDelta_a) <= 0.0000001){
+  //    console.log(" A line of two point are perpendicular to x-axis 1\n");
+      return true;
+    }
+    else if (Math.abs(yDelta_b) <= 0.0000001){
+  //    console.log(" A line of two point are perpendicular to x-axis 2\n");
+      return true;
+    }
+    else if (Math.abs(xDelta_a)<= 0.000000001){
+  //    console.log(" A line of two point are perpendicular to y-axis 1\n");
+      return true;
+    }
+    else if (Math.abs(xDelta_b)<= 0.000000001){
+  //    console.log(" A line of two point are perpendicular to y-axis 2\n");
+      return true;
+    }
+    else return false ;
   }
-  
-  // IsPerpendicular() assure that xDelta(s) are not zero
-  var aSlope=yDelta_a/xDelta_a; // 
-  var bSlope=yDelta_b/xDelta_b;
-  if (fabs(aSlope-bSlope) <= 0.000000001){  // checking whether the given points are colinear.  
-    console.log("The three pts are colinear\n");
-    return -1;
+
+  function calcCircle(pt1, pt2, pt3){
+    var yDelta_a= pt2.y - pt1.y;
+    var xDelta_a= pt2.x - pt1.x;
+    var yDelta_b= pt3.y - pt2.y;
+    var xDelta_b= pt3.x - pt2.x;
+
+    function distance( point1, point2 )
+    {
+      var xs = 0;
+      var ys = 0;
+
+      xs = point2.x - point1.x;
+      xs = xs * xs;
+
+      ys = point2.y - point1.y;
+      ys = ys * ys;
+
+      return Math.sqrt( xs + ys );
+    }
+    
+    if (Math.abs(xDelta_a) <= 0.000000001 && Math.abs(yDelta_b) <= 0.000000001){
+      console.log("Calc cirlce \n");
+      center.x= 0.5*(pt2.x + pt3.x);
+      center.y= 0.5*(pt1.y + pt2.y);
+      radius= distance(center,pt1);   // calc. radius
+     console.log(" Center: %f %f %f\n", center.x, center.y);
+     console.log(" radius: %f %f %f\n", distance(center,pt1), distance(center,pt2),distance(center,pt3));
+
+      return {x: center.x, y: center.y, radius: radius};
+    }
+    
+    // IsPerpendicular() assure that.xDelta(s) are not zero
+    var aSlope=yDelta_a/xDelta_a; // 
+    var bSlope=yDelta_b/xDelta_b;
+    if (Math.abs(aSlope-bSlope) <= 0.000000001){  // checking whether the given points are colinear.  
+      console.log("The three pts are colinear\n");
+
+      return {x: center.x, y: center.y, radius: radius};;
+    }
+
+    // calc center
+    center.x= (aSlope*bSlope*(pt1.y - pt3.y) + bSlope*(pt1.x + pt2.x)
+      - aSlope*(pt2.x+pt3.x) )/(2* (bSlope-aSlope) );
+    center.y = -1*(center.x - (pt1.x+pt2.x)/2)/aSlope +  (pt1.y+pt2.y)/2;
+
+    radius= distance(center,pt1);   // calc. radius
+   console.log(" Center: \n", center.x, center.y);
+   console.log(" Radius: \n", radius);
+   console.log(" radius: \n", distance(center,pt1), distance(center,pt2),distance(center,pt3));
+
+    return {x: center.x, y: center.y, radius: radius};
   }
-
-  // calc center
-  this.m_Center.m_x= (aSlope*bSlope*(pt1.m_y - pt3.m_y) + bSlope*(pt1.m_x + pt2 .m_x)
-    - aSlope*(pt2.m_x+pt3.m_x) )/(2* (bSlope-aSlope) );
-  this.m_Center.m_y = -1*(m_Center.x() - (pt1.x()+pt2.x())/2)/aSlope +  (pt1.y()+pt2.y())/2;
-  this.m_Center.m_z= pt1.m_z;
-
-  this.m_dRadius= length(&m_Center,pt1);   // calc. radius
- console.log(" Center: %f %f %f\n", m_Center.x(), m_Center.y(), m_Center.z());
- console.log(" radius: %f %f %f\n", length(&m_Center,pt1), length(&m_Center,pt2),length(&m_Center,pt3));
-  return this.m_dRadius;
-}
+};
--- www/js/index.js
+++ www/js/index.js
@@ -19,8 +19,8 @@ var myTimeZone = {
     this.paper.circle(p0.x, p0.y, 10);
     this.paper.circle(p1.x, p1.y, 10);
     this.paper.circle(p2.x, p2.y, 10);
-    var c = this.calcCircle(p0,p2,p1);
-    this.paper.circle(c.x, c.y, c.radius)
+    var c = circleFrom3Points(p0,p1,p2);
+    this.paper.circle(c.center.x, c.center.y, c.radius)
 
     // var s="";
     // _.forEach(this.times, function(d,k){
@@ -66,59 +66,7 @@ var myTimeZone = {
     return localTime;
   },
 
-  calcCircle: function(pt1, pt2, pt3){
-    var yDelta_a= pt2.y - pt1.y;
-    var xDelta_a= pt2.x - pt1.x;
-    var yDelta_b= pt3.y - pt2.y;
-    var xDelta_b= pt3.x - pt2.x;
-
-    var center = {x:-1, y:-1}, radius = -1;
-
-
-    function distance( point1, point2 )
-    {
-      var xs = 0;
-      var ys = 0;
-
-      xs = point2.x - point1.x;
-      xs = xs * xs;
-
-      ys = point2.y - point1.y;
-      ys = ys * ys;
-
-      return Math.sqrt( xs + ys );
-    }
-    
-    if (Math.abs(xDelta_a) <= 0.000000001 && Math.abs(yDelta_b) <= 0.000000001){
-      console.log("Calc cirlce \n");
-      center.x= 0.5*(pt2.x + pt3.x);
-      center.y= 0.5*(pt1.y + pt2.y);
-      radius= distance(center,pt1);   // calc. radius
-     console.log(" Center: %f %f %f\n", center.x, center.y);
-     console.log(" radius: %f %f %f\n", distance(center,pt1), distance(center,pt2),distance(center,pt3));
-
-      return {x: center.x, y: center.y, radius: radius};
-    }
-    
-    // IsPerpendicular() assure that.xDelta(s) are not zero
-    var aSlope=yDelta_a/xDelta_a; // 
-    var bSlope=yDelta_b/xDelta_b;
-    if (Math.abs(aSlope-bSlope) <= 0.000000001){  // checking whether the given points are colinear.  
-      console.log("The three pts are colinear\n");
-      return {x: center.x, y: center.y, radius: radius};;
-    }
-
-    // calc center
-    center.x= (aSlope*bSlope*(pt1.y - pt3.y) + bSlope*(pt1.x + pt2.x)
-      - aSlope*(pt2.x+pt3.x) )/(2* (bSlope-aSlope) );
-    center.y = -1*(center.x - (pt1.x+pt2.x)/2)/aSlope +  (pt1.y+pt2.y)/2;
-
-    radius= distance(center,pt1);   // calc. radius
-   console.log(" Center: \n", center.x, center.y);
-   console.log(" Radius: \n", radius);
-   console.log(" radius: \n", distance(center,pt1), distance(center,pt2),distance(center,pt3));
-    return {x: center.x, y: center.y, radius: radius};
-  }
+  
 };
 
 document.addEventListener('deviceready', myTimeZone.initialize(), false);
